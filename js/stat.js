ce0fdcd84e7eac47ff6b27f6429be32adb7beee4
Ekaterina <ekaterina.zink@gmail.com>
2018-09-23 21:10:06 -0300

    исправлены функции, добавлен random

--- js/stat.js
+++ js/stat.js
@@ -5,8 +5,6 @@ var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var GAP = 50;
-var FONT_GAP = 15;
-var TEXT_WIDTH = 50;
 var BAR_HEIGHT = 150;
 var BAR_WIDTH = 40;
 
@@ -33,24 +31,17 @@ window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!',  CLOUD_X + GAP/2, 40);
-  ctx.fillText('Список результатов:',  CLOUD_X + GAP/2, 60);
+  ctx.fillText('Ура вы победили!', CLOUD_X + GAP / 2, GAP - 10);
+  ctx.fillText('Список результатов:', CLOUD_X + GAP / 2, GAP + 10);
 
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < names.length; i++) {
-    if (names[i] !== 'Вы') {
-      ctx.fillStyle = '#000';
-      ctx.fillText(names[i], CLOUD_X + GAP + (GAP + BAR_WIDTH) * i, CLOUD_Y + CLOUD_HEIGHT - 15);
-      ctx.fillText(Math.round(times[i]), CLOUD_X + GAP + (GAP + BAR_WIDTH) * i, CLOUD_Y + CLOUD_HEIGHT - (BAR_HEIGHT * times[i]) / maxTime - 45);
-      ctx.fillStyle = 'blue';
-      ctx.fillRect(CLOUD_X + GAP + (GAP + BAR_WIDTH) * i, CLOUD_Y + CLOUD_HEIGHT - 35, BAR_WIDTH, -(BAR_HEIGHT * times[i]) / maxTime);
-    } else {
-      ctx.fillStyle = '#000';
-      ctx.fillText(names[i], CLOUD_X + GAP + (GAP + BAR_WIDTH) * i, CLOUD_Y + CLOUD_HEIGHT - 15);
-      ctx.fillText(Math.round(times[i]), CLOUD_X + GAP + (GAP + BAR_WIDTH) * i, CLOUD_Y + CLOUD_HEIGHT - (BAR_HEIGHT * times[i]) / maxTime - 45);
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-      ctx.fillRect(CLOUD_X + GAP + (GAP + BAR_WIDTH) * i, CLOUD_Y + CLOUD_HEIGHT - 35, BAR_WIDTH, -(BAR_HEIGHT * times[i]) / maxTime);
-    }
-}
+    ctx.fillStyle = '#000';
+    ctx.fillText(names[i], CLOUD_X + GAP + (GAP + BAR_WIDTH) * i, CLOUD_Y + CLOUD_HEIGHT - 15);
+    ctx.fillText(Math.round(times[i]), CLOUD_X + GAP + (GAP + BAR_WIDTH) * i, CLOUD_Y + CLOUD_HEIGHT - (BAR_HEIGHT * times[i]) / maxTime - 45);
+    ctx.fillStyle = (names[i] !== 'Вы' ? 'rgba(0, 0, Math.floor(Math.random() * 225), 1)' : 'rgba(255, 0, 0, 1)');
+    ctx.fillRect(CLOUD_X + GAP + (GAP + BAR_WIDTH) * i, CLOUD_Y + CLOUD_HEIGHT - 35, BAR_WIDTH, -(BAR_HEIGHT * times[i]) / maxTime);
+  }
+
 };
